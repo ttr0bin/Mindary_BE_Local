4eea620f9808496c86fcce76bd1828c527451560
ttr0bin <monica990131@gmail.com>
2024-08-02 18:41:46 +0900

    Fix : urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,8 +5,8 @@ urlpatterns = [
     # Original_Login
 
     # Kakao_Login
-    path('kakao/login/', kakao_login),
-    path('kakao/register/', kakao_register),
-    path('kakao/logout/', kakao_logout),
-    path('kakao/verify/', verify),
+    path('kakao/login', kakao_login),
+    path('kakao/register', kakao_register),
+    path('kakao/logout', kakao_logout),
+    path('kakao/verify', verify),
 ]
