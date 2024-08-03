58978a6701a426135a933fb8c2990c6696ffa8c2
mdy3722 <mdy3722@gmail.com>
2024-08-03 16:34:10 +0900

    Fix : 디버그 수정

--- mindary/settings.py
+++ mindary/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['43.200.87.30','43.201.89.165', 'localhost', '127.0.0.1']
 
