743cdf9d4fdd82bf893f8f1c026819138e6ea559
mdy3722 <mdy3722@gmail.com>
2024-08-04 02:24:25 +0900

    Fix : token 처리

--- mindary/settings.py
+++ mindary/settings.py
@@ -166,8 +166,8 @@ from datetime import timedelta
 
 # JWT
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(hours=6),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=7),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=30),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
     'AUTH_HEADER_TYPES': ('Bearer',),
