417b535a4c7b9d15c1c1d8cb40e514b99667cb06
mdy3722 <mdy3722@gmail.com>
2024-08-03 15:40:06 +0900

    Feat : 긴글 삭제 구현

--- records/views.py
+++ records/views.py
@@ -17,7 +17,6 @@ def archive(request):
     keyword         = request.GET.get('keyword', None)
 
     # 레코드 쿼리셋 필터링 및 정렬
-    records = Record.objects.all()
     records = Record.objects.filter(writer = request.user)
     if category:
         records = records.filter(category=category)
@@ -53,45 +52,41 @@ def detail_archive(request, id):
       < 주의 >
       긴글모드에서 진행!!! 모아보기랑 관련 없음!!!
 """
-# 긴글 수정
-@api_view(['PATCH'])
+# 긴글 수정 및 삭제
+@api_view(['PATCH', 'DELETE'])
 @permission_classes([IsAuthenticated])   # 로그인 유저만 권한 있음
 def modify_record(request, id):
     date_query_param = request.GET.get('date', None)
     mode = request.GET.get('mode', None)
-
     if not date_query_param:
         return Response({"error": "쿼리 파라미터가 없습니다."}, status=status.HTTP_400_BAD_REQUEST)
     if mode != 'record':
         return Response({"error": "긴글 모드를 이용해주세요!"}, status=status.HTTP_400_BAD_REQUEST)
-
-    try:     # 이때 date는 디비에 있는 데이터의 created_at과 비교 할 날짜
-        selected_date = datetime.strptime(date_query_param, "%Y-%m-%d").date()
-    except ValueError:
-        return Response({"error": "쿼리 파라미터 형식을 지켜주세요. YYYY-MM-DD"}, status=status.HTTP_400_BAD_REQUEST)
-    
-    # today = date.today()
-    # if selected_date != today:
-    #     return Response({"error": "오늘의 기록만 수정 할 수 있어요!"}, status=status.HTTP_400_BAD_REQUEST)
 
     try:
         record = Record.objects.get(id=id)
     except Record.DoesNotExist:
         return Response({"error": "작성한 글이 없습니다!"}, status=status.HTTP_404_NOT_FOUND)
     
-    data = request.data
-    if not any(field in data for field in ['title', 'content', 'liked']):
-        return Response({"error": "수정된 것이 없습니다."}, status=status.HTTP_400_BAD_REQUEST)
-    
-    if 'title' in data:
-        record.title = data['title']
-    if 'content' in data:
-        record.content = data['content']
-    if 'liked' in data:
-        record.liked = data['liked']
-    
-    record.edited_at = datetime.now()
-    record.save()
+    match request.method:
+        case 'PATCH':   
+            data = request.data
+            if not any(field in data for field in ['title', 'content', 'liked']):
+                return Response({"error": "수정된 것이 없습니다."}, status=status.HTTP_400_BAD_REQUEST)
+            
+            if 'title' in data:
+                record.title = data['title']
+            if 'content' in data:
+                record.content = data['content']
+            if 'liked' in data:
+                record.liked = data['liked']
+            
+            record.edited_at = datetime.now()
+            record.save()
 
-    record_serializer = RecordSerializer(record)
-    return Response(record_serializer.data, status=status.HTTP_200_OK)
+            record_serializer = RecordSerializer(record)
+            return Response(record_serializer.data, status=status.HTTP_200_OK)
+
+        case 'DELETE':
+            record.delete()
+            return Response({"message": "삭제 성공!"}, status=status.HTTP_200_OK)
